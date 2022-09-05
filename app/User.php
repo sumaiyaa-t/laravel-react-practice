86e1940fc873c8429b0c5f0072d46ccbca09b4c2
Tahmid <tahmid@pop-os.localdomain>
2022-09-05 18:12:08 +0600

    Authentication system implemented

--- app/User.php
+++ app/User.php
@@ -6,9 +6,11 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+use Laravel\Sanctum\HasApiTokens; //use kora hoise
+
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable,Notifiable,HasApiTokens; //using HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
